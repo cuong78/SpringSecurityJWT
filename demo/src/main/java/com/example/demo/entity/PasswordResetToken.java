c0fdf74bbc315b8b216c52d0d210cadff4453789
anhCuong <cuongcaoleanh@gmail.com>
2025-02-19 21:22:29 +0700

    update

--- demo/src/main/java/com/example/demo/entity/PasswordResetToken.java
+++ demo/src/main/java/com/example/demo/entity/PasswordResetToken.java
@@ -1,8 +1,12 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
-import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Date;
+@Getter
+@Setter
 @Entity
 public class PasswordResetToken {
 
@@ -18,36 +22,4 @@ public class PasswordResetToken {
 
     private Date expiryDate;
 
-    // Getters và Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    public Date getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(Date expiryDate) {
-        this.expiryDate = expiryDate;
-    }
 }
